df0e41ff82f0cd4607041e79196cee31b60ff1b6
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-12 13:53:47 +0700

    event of editor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import CKEditor from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 const App = () => {
     return (
-        <div>
-            <h1>CKEditor With React</h1>
+        <div className="container">
+            <h1 className="text-center">CKEditor With React</h1>
+            <div className="row">
+                <div className="col-12 my-10 my-3">
+                    <CKEditor
+                        editor={ ClassicEditor }
+                        data="<p>CKEditor With React</p>"
+                        onInit={ editor => {
+                            console.log( 'Editor is ready to use!', editor )
+                        }}
+                        onChange={ ( event, editor ) => {
+                            const data = editor.getData()
+                            console.log( { event, editor, data } )
+                        }}
+                        onBlur={ editor => {
+                            console.log( 'Blur.', editor )
+                        }}
+                        onFocus={ editor => {
+                            console.log( 'Focus.', editor )
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
